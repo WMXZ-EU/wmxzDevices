aa6be3a7b89fde7ee0484a9560679a347f55c6e2
WMXZ-EU <walter@wmxz.eu>
2016-04-26 17:48:32 +0200

    initial upload
    Added Compass files

---
+++ src/compass.c
@@ -0,0 +1,213 @@
+#include <stdint.h>
+#include "core_pins.h"
+//
+#include "iic.h"
+#include "jobs.h"
+#include "pdb.h"
+#include "compass.h"
+
+int m_compass_cnt=0;
+int m_compass_done=0;
+I2C_FUNC m_compass_funct=0;
+
+int usb_serial_putchar(uint8_t c);
+int usb_serial_write(const void *buffer, uint32_t size);
+int usb_serial_write_buffer_free(void);
+void usb_serial_flush_output(void);
+
+void logg(uint8_t c) {	usb_serial_putchar(c); usb_serial_flush_output();}
+
+typedef struct
+{ 	uint8_t address;
+	uint8_t reg;
+	uint8_t *data;
+	uint8_t ndat;
+} COMPASS_STR;
+
+COMPASS_STR *m_compass;
+
+#define NSENS (4)
+COMPASS_STR compass_seq[]=
+{	{ gyroAddress, L3G_OUT_X_L | (1<<7), 0, 6},
+	{ accAddress,  ACC_BASE    | (1<<7), 0, 6},
+	{ magAddress,  MAG_BASE    | (1<<7), 0, 6},
+	{ magAddress,  TMP_BASE    | (1<<7), 0, 2}
+};
+
+void compass_initRead(Ptr cd,  Ptr dd);
+void compass_readData(Ptr cd, Ptr dd);
+void compass_TX_CB(Ptr d, int n);
+void compass_RX_CB(Ptr d, int n);
+
+//(Fxn_t funct, Ptr state, Ptr data, int nice, uint32_t uDelay)
+void compass_TX_CB(Ptr d, int n)
+{	DELAY_add(compass_readData,0,0,-1,25);
+}
+
+void compass_RX_CB(Ptr d, int n)
+{	//
+	m_compass=&compass_seq[m_compass_cnt++];
+	//
+	if(m_compass_cnt<=NSENS)
+		DELAY_add(compass_initRead,0,0,-1,1000); // delay between compass readings in usec
+	else
+		m_compass_funct(0,0);
+}
+
+void compass_initRead(Ptr cd,  Ptr dd)
+{	m_compass->data[0]=m_compass->reg; // add continuous read-out mode
+	i2c_send(m_compass->address,m_compass->data,1,0, compass_TX_CB);
+}
+
+void compass_readData(Ptr cd, Ptr dd)
+{	i2c_receive(m_compass->address,m_compass->data,m_compass->ndat,1,compass_RX_CB);
+}
+
+void compass_getData(uint8_t *data, I2C_FUNC funct)
+{	int ii;
+	m_compass_funct=funct;
+	// adjust data addresses
+	compass_seq[0].data=data;
+	for(ii=1;ii<NSENS;ii++)
+	{	compass_seq[ii].data=compass_seq[ii-1].data+compass_seq[ii-1].ndat;
+	}
+	// start compass processing sequence
+	m_compass_cnt=0;
+	m_compass=&compass_seq[m_compass_cnt++];
+	//
+	compass_initRead(0,0);
+}
+
+//--------------------------------------------------------------------------------------------------
+/*
+uint8_t *readData(uint8_t address, uint8_t reg, uint8_t *data, uint8_t ndat)
+{
+  data[0]=reg; // add continuous read-out mode
+	i2c_send(address,data,1,0,0); while(!i2c_done());
+	delayMicroseconds(25);
+	i2c_receive(address,data,ndat,1,0); while(!i2c_done());
+  return data;
+}
+*/
+uint8_t *readData(uint8_t address, uint8_t reg, uint8_t *data, uint8_t ndat)
+{
+  data[0]=reg; // add continuous read-out mode
+  IIC_write(data, 1, address,0);
+  IIC_read(data, ndat, address,1);
+  return data;
+}
+
+//=====================acc=================
+uint8_t accInit(void)
+{ uint8_t retVal=0;
+//
+    retVal=readRegister(accAddress,WHO_AM_I);
+    
+    // Accelerometer
+    // ODR = 0110 (100 Hz ODR); LPen = 0 (normal mode); Zen = Yen = Xen = 1 (all axes enabled)
+    writeRegister(accAddress,CTRL1, 0x67); //100 Hz all accel
+	writeRegister(accAddress,CTRL2, 0xC0); //50 Hz LP +-2g
+
+  return retVal;
+}
+
+inline uint8_t *accRead( uint8_t *data)
+{ return readData(accAddress, ACC_BASE | (1<<7), data,6); //read continuous 6 values
+}
+
+//=====================mag=================
+uint8_t magInit(void)
+{ uint8_t retVal=0;
+    retVal=readRegister(magAddress, WHO_AM_I);
+  
+    // Magnetometer
+	writeRegister(accAddress,CTRL5, 0x94); //100 Hz update
+    writeRegister(accAddress,CTRL6, 0x00); //D-
+    writeRegister(accAddress,CTRL7, 0x00); //D- continuous mode
+  
+return retVal;
+}
+
+inline uint8_t *magRead( uint8_t *data)
+{ return readData(magAddress,MAG_BASE | (1<<7),data,6); // add continuous read-out mode
+}
+
+inline uint8_t *tempRead(uint8_t *data)
+{ return readData(magAddress,TMP_BASE | (1<<7),data,2); // add continuous read-out mode
+}
+
+//=====================gyro=================
+
+uint8_t gyroInit(void)
+{ uint8_t retVal;
+
+  retVal=readRegister(gyroAddress,L3G_WHO_AM_I);
+  writeRegister(gyroAddress,L3G_CTRL_REG1,0x3F); // normal power mode, all axes enabled, 100 Hz fs,  25 Hz BW
+  writeRegister(gyroAddress,L3G_CTRL_REG4,0x10); // 500 dps full scale
+  return retVal;
+}
+
+inline uint8_t *gyroRead(uint8_t *data)
+{  return readData(gyroAddress,L3G_OUT_X_L | (1<<7),data,6); // add continuous read-out mode
+}
+
+//======================== compass ===================
+uint8_t compassInit(void)
+{ int ret1,ret2,ret3;
+  ret1=gyroInit(); 
+  ret2=accInit();
+  ret3=magInit();
+  //
+  return ret3;
+}
+
+#ifdef LSM303D
+	void getGyro(uint16_t *data){ gyroRead((uint8_t *)data);}
+	void getAccel(uint16_t *data){ accRead((uint8_t *)data);}
+	void getMag(uint16_t *data){ magRead((uint8_t *)data);}
+	void getTemp(uint16_t *data){ tempRead((uint8_t *)data);}
+#else
+	void getGytro(int16_t *data)
+	{	int ii;
+		int16_t buf[4]; uint8_t *tt=(uint8_t *) &buf[0];
+		gyroRead(tt); for(ii=0;ii<3;ii++) data[ii]=buf[ii];
+	}
+	void getAccel(int16_t *data)
+	{	int ii;
+		int16_t buf[4]; uint8_t *tt=(uint8_t *) &buf[0];
+		accRead(tt); for(ii=0;ii<3;ii++) data[3+ii]=buf[ii]>>4;
+	}
+	void getMag(int16_t *data)
+	{	int ii;
+		int16_t buf[4]; uint8_t *tt=(uint8_t *) &buf[0];
+		magRead(tt); for(ii=0;ii<3;ii++) data[6+ii]=ByteSwap(buf[ii]);//(int16_t)(tt[jj++]<<8 | tt[jj++]);
+	}
+	void getTemp(int16_t *data)
+	{	int ii;
+		int16_t buf[4]; uint8_t *tt=(uint8_t *) &buf[0];
+		tempRead(tt); data[9]=ByteSwap(buf[0])>>4;// ((int16_t)(tt[0]<<8 | tt[1]))>>4;
+	}
+#endif
+
+uint8_t compassRead(int16_t *data)
+{ int ii;
+  int16_t buf[4]; uint8_t *tt=(uint8_t *) &buf[0];
+
+  // data are LSB first no need for byte swap
+  gyroRead(tt); for(ii=0;ii<3;ii++) data[ii]=buf[ii];
+  
+  #ifdef LSM303D
+	  accRead(tt); for(ii=0;ii<3;ii++) data[3+ii]=buf[ii];
+	  magRead(tt); for(ii=0;ii<3;ii++) data[6+ii]=buf[ii];
+	  tempRead(tt); data[9]=buf[0];
+  #else
+	  accRead(tt); for(ii=0;ii<3;ii++) data[3+ii]=buf[ii]>>4;
+	  magRead(tt); for(ii=0;ii<3;ii++) data[6+ii]=ByteSwap(buf[ii]);//(int16_t)(tt[jj++]<<8 | tt[jj++]);
+	  tempRead(tt); data[9]=ByteSwap(buf[0])>>4;// ((int16_t)(tt[0]<<8 | tt[1]))>>4;
+  #endif
+  //
+  return 1;
+}
+
+
+
---
+++ src/compass.h
@@ -0,0 +1,333 @@
+#ifndef COMPASS_H
+#define COMPASS_H
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+//miniIMU9.c
+// Uncomment the below line to use this axis definition: 
+   // X axis pointing forward
+   // Y axis pointing to the right 
+   // and Z axis pointing down.
+// Positive pitch : nose up
+// Positive roll : right wing down
+// Positive yaw : clockwise
+
+/*
+int SENSOR_SIGN[9] = {1,1,1,-1,-1,-1,1,1,1}; //Correct directions x,y,z - gyro, accelerometer, magnetometer
+*/
+
+// Uncomment the below line to use this axis definition: 
+   // X axis pointing forward
+   // Y axis pointing to the left 
+   // and Z axis pointing up.
+// Positive pitch : nose down
+// Positive roll : right wing down
+// Positive yaw : counterclockwise
+//int SENSOR_SIGN[9] = {1,-1,-1,-1,1,1,1,-1,-1}; //Correct directions x,y,z - gyro, accelerometer, magnetometer
+
+
+// LSM303 accelerometer: 8 g sensitivity
+// 3.9 mg/digit; 1 g = 256
+#define GRAVITY 256  //this equivalent to 1G in the raw data coming from the accelerometer 
+
+#define ToRad(x) ((x)*0.01745329252)  // *pi/180
+#define ToDeg(x) ((x)*57.2957795131)  // *180/pi
+
+// L3G4200D gyro: 2000 dps full scale
+// 70 mdps/digit; 1 dps = 0.07
+#define Gyro_Gain_X 0.07 //X axis Gyro gain
+#define Gyro_Gain_Y 0.07 //Y axis Gyro gain
+#define Gyro_Gain_Z 0.07 //Z axis Gyro gain
+#define Gyro_Scaled_X(x) ((x)*ToRad(Gyro_Gain_X)) //Return the scaled ADC raw data of the gyro in radians for second
+#define Gyro_Scaled_Y(x) ((x)*ToRad(Gyro_Gain_Y)) //Return the scaled ADC raw data of the gyro in radians for second
+#define Gyro_Scaled_Z(x) ((x)*ToRad(Gyro_Gain_Z)) //Return the scaled ADC raw data of the gyro in radians for second
+
+// LSM303 magnetometer calibration constants; use the Calibrate example from
+// the Pololu LSM303 library to find the right values for your board
+#define M_X_MIN -421
+#define M_Y_MIN -639
+#define M_Z_MIN -238
+#define M_X_MAX 424
+#define M_Y_MAX 295
+#define M_Z_MAX 472
+
+#define Kp_ROLLPITCH 0.02
+#define Ki_ROLLPITCH 0.00002
+#define Kp_YAW 1.2
+#define Ki_YAW 0.00002
+
+/*For debugging purposes*/
+//OUTPUTMODE=1 will print the corrected data, 
+//OUTPUTMODE=0 will print uncorrected data of the gyros (with drift)
+#define OUTPUTMODE 1
+
+//#define PRINT_DCM 0     //Will print the whole direction cosine matrix
+#define PRINT_ANALOGS 0 //Will print the analog raw data
+#define PRINT_EULER 1   //Will print the Euler angles Roll, Pitch and Yaw
+
+#define STATUS_LED 13 
+/*
+float G_Dt=0.02;    // Integration time (DCM algorithm)  We will run the integration loop at 50Hz if possible
+
+long timer=0;   //general purpose timer
+long timer_old;
+long timer24=0; //Second timer used to print values 
+int AN[6]; //array that stores the gyro and accelerometer data
+int AN_OFFSET[6]={0,0,0,0,0,0}; //Array that stores the Offset of the sensors
+
+int gyro_x;
+int gyro_y;
+int gyro_z;
+int accel_x;
+int accel_y;
+int accel_z;
+int magnetom_x;
+int magnetom_y;
+int magnetom_z;
+float c_magnetom_x;
+float c_magnetom_y;
+float c_magnetom_z;
+float MAG_Heading;
+
+float Accel_Vector[3]= {0,0,0}; //Store the acceleration in a vector
+float Gyro_Vector[3]= {0,0,0};//Store the gyros turn rate in a vector
+float Omega_Vector[3]= {0,0,0}; //Corrected Gyro_Vector data
+float Omega_P[3]= {0,0,0};//Omega Proportional correction
+float Omega_I[3]= {0,0,0};//Omega Integrator
+float Omega[3]= {0,0,0};
+
+// Euler angles
+float roll;
+float pitch;
+float yaw;
+
+float errorRollPitch[3]= {0,0,0}; 
+float errorYaw[3]= {0,0,0};
+
+unsigned int counter=0;
+unsigned char gyro_sat=0;
+*/
+
+//GYRO
+#define L3GD20_ADDRESS_SA0_LOW    (0xD4 >> 1)
+#define L3GD20_ADDRESS_SA0_HIGH   (0xD6 >> 1)
+
+// register addresses
+
+#define L3G_WHO_AM_I      0x0F
+
+#define L3G_CTRL_REG1     0x20
+#define L3G_CTRL_REG2     0x21
+#define L3G_CTRL_REG3     0x22
+#define L3G_CTRL_REG4     0x23
+#define L3G_CTRL_REG5     0x24
+#define L3G_REFERENCE     0x25
+#define L3G_OUT_TEMP      0x26
+#define L3G_STATUS_REG    0x27
+
+#define L3G_OUT_X_L       0x28
+#define L3G_OUT_X_H       0x29
+#define L3G_OUT_Y_L       0x2A
+#define L3G_OUT_Y_H       0x2B
+#define L3G_OUT_Z_L       0x2C
+#define L3G_OUT_Z_H       0x2D
+
+#define L3G_FIFO_CTRL_REG 0x2E
+#define L3G_FIFO_SRC_REG  0x2F
+
+#define L3G_INT1_CFG      0x30
+#define L3G_INT1_SRC      0x31
+#define L3G_INT1_THS_XH   0x32
+#define L3G_INT1_THS_XL   0x33
+#define L3G_INT1_THS_YH   0x34
+#define L3G_INT1_THS_YL   0x35
+#define L3G_INT1_THS_ZH   0x36
+#define L3G_INT1_THS_ZL   0x37
+#define L3G_INT1_DURATION 0x38
+#define L3G_LOW_ODR       0x39
+
+
+    // register addresses
+    enum regAddr
+    {
+      TEMP_OUT_L        = 0x05, // D
+      TEMP_OUT_H        = 0x06, // D
+
+      STATUS_M          = 0x07, // D
+
+      INT_CTRL_M        = 0x12, // D
+      INT_SRC_M         = 0x13, // D
+      INT_THS_L_M       = 0x14, // D
+      INT_THS_H_M       = 0x15, // D
+
+      OFFSET_X_L_M      = 0x16, // D
+      OFFSET_X_H_M      = 0x17, // D
+      OFFSET_Y_L_M      = 0x18, // D
+      OFFSET_Y_H_M      = 0x19, // D
+      OFFSET_Z_L_M      = 0x1A, // D
+      OFFSET_Z_H_M      = 0x1B, // D
+      REFERENCE_X       = 0x1C, // D
+      REFERENCE_Y       = 0x1D, // D
+      REFERENCE_Z       = 0x1E, // D
+
+      CTRL0             = 0x1F, // D
+      CTRL1             = 0x20, // D
+      CTRL_REG1_A       = 0x20, // DLH, DLM, DLHC
+      CTRL2             = 0x21, // D
+      CTRL_REG2_A       = 0x21, // DLH, DLM, DLHC
+      CTRL3             = 0x22, // D
+      CTRL_REG3_A       = 0x22, // DLH, DLM, DLHC
+      CTRL4             = 0x23, // D
+      CTRL_REG4_A       = 0x23, // DLH, DLM, DLHC
+      CTRL5             = 0x24, // D
+      CTRL_REG5_A       = 0x24, // DLH, DLM, DLHC
+      CTRL6             = 0x25, // D
+      CTRL_REG6_A       = 0x25, // DLHC
+      HP_FILTER_RESET_A = 0x25, // DLH, DLM
+      CTRL7             = 0x26, // D
+      REFERENCE_A       = 0x26, // DLH, DLM, DLHC
+      STATUS_A          = 0x27, // D
+      STATUS_REG_A      = 0x27, // DLH, DLM, DLHC
+
+      OUT_X_L_A         = 0x28,
+      OUT_X_H_A         = 0x29,
+      OUT_Y_L_A         = 0x2A,
+      OUT_Y_H_A         = 0x2B,
+      OUT_Z_L_A         = 0x2C,
+      OUT_Z_H_A         = 0x2D,
+
+      FIFO_CTRL         = 0x2E, // D
+      FIFO_CTRL_REG_A   = 0x2E, // DLHC
+      FIFO_SRC          = 0x2F, // D
+      FIFO_SRC_REG_A    = 0x2F, // DLHC
+
+      IG_CFG1           = 0x30, // D
+      INT1_CFG_A        = 0x30, // DLH, DLM, DLHC
+      IG_SRC1           = 0x31, // D
+      INT1_SRC_A        = 0x31, // DLH, DLM, DLHC
+      IG_THS1           = 0x32, // D
+      INT1_THS_A        = 0x32, // DLH, DLM, DLHC
+      IG_DUR1           = 0x33, // D
+      INT1_DURATION_A   = 0x33, // DLH, DLM, DLHC
+      IG_CFG2           = 0x34, // D
+      INT2_CFG_A        = 0x34, // DLH, DLM, DLHC
+      IG_SRC2           = 0x35, // D
+      INT2_SRC_A        = 0x35, // DLH, DLM, DLHC
+      IG_THS2           = 0x36, // D
+      INT2_THS_A        = 0x36, // DLH, DLM, DLHC
+      IG_DUR2           = 0x37, // D
+      INT2_DURATION_A   = 0x37, // DLH, DLM, DLHC
+
+      CLICK_CFG         = 0x38, // D
+      CLICK_CFG_A       = 0x38, // DLHC
+      CLICK_SRC         = 0x39, // D
+      CLICK_SRC_A       = 0x39, // DLHC
+      CLICK_THS         = 0x3A, // D
+      CLICK_THS_A       = 0x3A, // DLHC
+      TIME_LIMIT        = 0x3B, // D
+      TIME_LIMIT_A      = 0x3B, // DLHC
+      TIME_LATENCY      = 0x3C, // D
+      TIME_LATENCY_A    = 0x3C, // DLHC
+      TIME_WINDOW       = 0x3D, // D
+      TIME_WINDOW_A     = 0x3D, // DLHC
+
+      Act_THS           = 0x3E, // D
+      Act_DUR           = 0x3F, // D
+
+      CRA_REG_M         = 0x00, // DLH, DLM, DLHC
+      CRB_REG_M         = 0x01, // DLH, DLM, DLHC
+      MR_REG_M          = 0x02, // DLH, DLM, DLHC
+
+      SR_REG_M          = 0x09, // DLH, DLM, DLHC
+      IRA_REG_M         = 0x0A, // DLH, DLM, DLHC
+      IRB_REG_M         = 0x0B, // DLH, DLM, DLHC
+      IRC_REG_M         = 0x0C, // DLH, DLM, DLHC
+
+      WHO_AM_I_M        = 0x0F, // DLM
+      WHO_AM_I          = 0x0F, // D
+
+      TEMP_OUT_H_M      = 0x31, // DLHC
+      TEMP_OUT_L_M      = 0x32, // DLHC
+
+
+      // dummy addresses for registers in different locations on different devices;
+      // the library translates these based on device type
+      // value with sign flipped is used as index into translated_regs array
+
+      OUT_X_H_M         = -1,
+      OUT_X_L_M         = -2,
+      OUT_Y_H_M         = -3,
+      OUT_Y_L_M         = -4,
+      OUT_Z_H_M         = -5,
+      OUT_Z_L_M         = -6,
+      // update dummy_reg_count if registers are added here!
+
+      // device-specific register addresses
+
+      DLH_OUT_X_H_M     = 0x03,
+      DLH_OUT_X_L_M     = 0x04,
+      DLH_OUT_Y_H_M     = 0x05,
+      DLH_OUT_Y_L_M     = 0x06,
+      DLH_OUT_Z_H_M     = 0x07,
+      DLH_OUT_Z_L_M     = 0x08,
+
+      DLM_OUT_X_H_M     = 0x03,
+      DLM_OUT_X_L_M     = 0x04,
+      DLM_OUT_Z_H_M     = 0x05,
+      DLM_OUT_Z_L_M     = 0x06,
+      DLM_OUT_Y_H_M     = 0x07,
+      DLM_OUT_Y_L_M     = 0x08,
+
+      DLHC_OUT_X_H_M    = 0x03,
+      DLHC_OUT_X_L_M    = 0x04,
+      DLHC_OUT_Z_H_M    = 0x05,
+      DLHC_OUT_Z_L_M    = 0x06,
+      DLHC_OUT_Y_H_M    = 0x07,
+      DLHC_OUT_Y_L_M    = 0x08,
+
+      D_OUT_X_L_M       = 0x08,
+      D_OUT_X_H_M       = 0x09,
+      D_OUT_Y_L_M       = 0x0A,
+      D_OUT_Y_H_M       = 0x0B,
+      D_OUT_Z_L_M       = 0x0C,
+      D_OUT_Z_H_M       = 0x0D
+    };
+
+#define D_SA0_HIGH_ADDRESS              0b0011101 // D with SA0 high
+#define D_SA0_LOW_ADDRESS               0b0011110 // D with SA0 low or non-D magnetometer
+#define NON_D_MAG_ADDRESS               0b0011110 // D with SA0 low or non-D magnetometer
+#define NON_D_ACC_SA0_LOW_ADDRESS       0b0011000 // non-D accelerometer with SA0 low
+#define NON_D_ACC_SA0_HIGH_ADDRESS      0b0011001 // non-D accelerometer with SA0 high
+
+#define D_WHO_ID    0x49
+#define DLM_WHO_ID  0x3C
+
+#define  gyroAddress L3GD20_ADDRESS_SA0_HIGH
+
+#ifdef LSM303D // is new board
+	#define accAddress D_SA0_HIGH_ADDRESS // D
+	#define magAddress D_SA0_HIGH_ADDRESS // D
+#else
+	#define accAddress NON_D_ACC_SA0_HIGH_ADDRESS // non-D
+	#define magAddress NON_D_MAG_ADDRESS // non-D
+#endif
+
+#ifdef LSM303D
+	#define ACC_BASE 0x28 
+	#define MAG_BASE 0x08 
+	#define TMP_BASE 0x05
+#else
+	//#define MAG_BASE DLHC_OUT_X_H_M
+	#define ACC_BASE 0x28
+	#define MAG_BASE 0x03
+	#define TMP_BASE 0x31
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
